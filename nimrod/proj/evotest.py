662db8a03866c6a255af88e1bbf8bf6d134010c2
jprmoisakis <jprmoisakis@gmail.com>
2019-07-20 03:13:44 -0300

    finishes repository analysis based on csv

--- nimrod/proj/evotest.py
+++ nimrod/proj/evotest.py
@@ -34,10 +34,15 @@ class evotest:
         self.maven = Maven(self.java, get_config()['maven_home'])
         self.evosuite_diff_params = None
         self.suite_evosuite_diff =None
+
+
+        # for now defines most of the folders configuration
+
         self.tests_src = "/home/jprm/Documents/IC/nimrod-hunor/nimrod/proj/output"
 
         self.project = GitProject("/home/jprm/Documents/IC/ex/example-project-evosuite")
         self.projects_folder = "/home/jprm/Documents/IC/ex/Projects/"
+        self.path_hash_csv = "/home/jprm/Documents/IC/ex/mergeScenarios.csv"
 
     def gen_evosuite_diff(self):
         evosuite = Evosuite(
@@ -68,7 +73,7 @@ class evotest:
         data = [(commit.get_base_hash(), "base"), (commit.get_left_hash(), "left"), (commit.get_right_hash(), "right"), (commit.get_merge_hash(),"merge")]
         for hash in data:
             self.project.checkout_on_commit(hash[0])
-            a =self.maven.compile(self.project.get_path_local_project(), 120, clean=True,install=True)
+            a =self.maven.compile(self.project.get_path_local_project(), 120, clean=True, install=True)
             print(a)
             self.maven.save_dependencies(self.project.get_path_local_project())
             dst = self.projects_folder + self.project.get_project_name() + "/" + hash[1]
@@ -78,22 +83,77 @@ class evotest:
             shutil.copytree(self.project.get_path_local_project(), dst)
 
 
-    def generateDependenciesPath(self):
-        self.projects_folder + self.project.get_project_name() + "/"
+    def generateDependenciesPath(self,commit_type):
+        project_folder = self.projects_folder + self.project.get_project_name() + "/"
+
+        dependencies = [(x[0], x[2]) for x in os.walk(project_folder+ commit_type+"/target/dependency/")]
+        depPath = dependencies[0][0]
+        finalPath=""
+        print(depPath)
+        for dependency in dependencies[0][1]:
+            finalPath = finalPath + depPath + dependency + ":"
+
+        finalPath = depPath + ":"+ finalPath +project_folder + commit_type + "/target/classes/"
+
+        return finalPath
+
 
 if __name__ == '__main__':
 
     evo = evotest()
 
 
-    merge = MergeScenario("/home/jprm/Documents/IC/ex/example-project-evosuite", "/home/jprm/Documents/IC/ex/mergeScenarios.csv")
+    merge = MergeScenario(evo.project.get_path_local_project, evo.path_hash_csv)
     merge_scenarios = merge.get_merge_scenarios()
-
     for scenario in merge_scenarios:
         evo.analyze_commit(scenario)
 
+    evo.dRegCp = evo.generateDependenciesPath("base")
+    evo.classes_dir = evo.generateDependenciesPath("left")
+    evo.mergeDir = evo.generateDependenciesPath("merge")
+
+    thread_evosuite_diff = threading.Thread(target=evo.gen_evosuite_diff)
+    print("waiting analisys finish")
+    thread_evosuite_diff.start()
+    thread_evosuite_diff.join()
+    print("ended")
+
+    time.sleep(5)
+    test_result = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.dRegCp)#fail on base
+    #time.sleep(4)
+    test_result2 = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.classes_dir)#pass on left
+    #time.sleep(4)
+
+    test_result3 = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.mergeDir)#fail on merge
+
+
+
+    #test_result = evo.try_evosuite_diff(evo.dRegCp, evo.sut_class, evo.classes_dir) #ok
+
+    #test_result = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.classes_dir) #ok
+
+    #test_result = evo.try_evosuite_diff(evo.dRegCp, evo.sut_class, evo.dRegCp) #notok ----
+
+    #test_result = evo.try_evosuite_diff(evo.mergeDir, evo.sut_class, evo.mergeDir) #ok
+
+    #test_result = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.mergeDir)#ok
+
+    #test_result = evo.try_evosuite_diff(evo.dRegCp, evo.sut_class, evo.mergeDir)#ok
+
+    #test_result = evo.try_evosuite_diff(evo.mergeDir, evo.sut_class, evo.dRegCp)#notok ----
+
+    #test_result = evo.try_evosuite_diff(evo.mergeDir, evo.sut_class, evo.classes_dir)#ok
+
+
+
+    print(test_result)
+    print(test_result2)
+    print(test_result3)
+
+
+
 '''
-    evo = evotest()
+    evo = evotest()/
     mvn = evo.maven
     project = GitProject("/home/jprm/Documents/IC/ex/example-project-evosuite")
 
@@ -139,49 +199,6 @@ if __name__ == '__main__':
     merge_dir = shutil.copytree(project.get_path_local_project(), dst)
 
 
-'''
-
-'''
-    evo = evotest()
-    #evo.gen_evosuite_diff()
-    thread_evosuite_diff = threading.Thread(target=evo.gen_evosuite_diff)
-
-    print("waiting analisys finish")
-    thread_evosuite_diff.start()
-    thread_evosuite_diff.join()
-    print("ended")
-
-    time.sleep(5)
-    test_result = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.dRegCp)#notok ----
-    #time.sleep(4)
-    test_result2 = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.classes_dir)#notok ----
-    #time.sleep(4)
-
-    test_result3 = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.mergeDir)#notok ----
-
-
-
-    #test_result = evo.try_evosuite_diff(evo.dRegCp, evo.sut_class, evo.classes_dir) #ok
-
-    #test_result = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.classes_dir) #ok
-
-    #test_result = evo.try_evosuite_diff(evo.dRegCp, evo.sut_class, evo.dRegCp) #notok ----
-
-    #test_result = evo.try_evosuite_diff(evo.mergeDir, evo.sut_class, evo.mergeDir) #ok
-
-    #test_result = evo.try_evosuite_diff(evo.classes_dir, evo.sut_class, evo.mergeDir)#ok
-
-    #test_result = evo.try_evosuite_diff(evo.dRegCp, evo.sut_class, evo.mergeDir)#ok
-
-    #test_result = evo.try_evosuite_diff(evo.mergeDir, evo.sut_class, evo.dRegCp)#notok ----
-
-    #test_result = evo.try_evosuite_diff(evo.mergeDir, evo.sut_class, evo.classes_dir)#ok
-
-
-
-    print(test_result)
-    print(test_result2)
-    print(test_result3)
 
     #if test_result.fail_tests > 0 or test_result.timeout:
     #    results = evo.create_nimrod_result(test_result, True, 'evosuite')
