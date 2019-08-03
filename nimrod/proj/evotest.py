07e8eec72b4bb9cc7a23db046ec21faa9d82e816
jprmoisakis <jprmoisakis@gmail.com>
2019-08-03 15:34:19 -0300

    change generated tests directory structure

--- nimrod/proj/evotest.py
+++ nimrod/proj/evotest.py
@@ -26,9 +26,9 @@ class evotest:
 
 
     def __init__(self):
-        self.dRegCp = '/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/apiguardian-api-1.0.0.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/commons-lang3-3.0.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/hamcrest-core-1.3.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/junit-4.13-beta-1.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/junit-jupiter-api-5.0.3.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/junit-platform-commons-1.0.3.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/dependency/opentest4j-1.0.0.jar:/home/jprm/Documents/IC/example-project-evosuite/base/target/classes/'
-        self.classes_dir = '/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/apiguardian-api-1.0.0.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/commons-lang3-3.0.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/hamcrest-core-1.3.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/junit-4.13-beta-1.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/junit-jupiter-api-5.0.3.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/junit-platform-commons-1.0.3.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/dependency/opentest4j-1.0.0.jar:/home/jprm/Documents/IC/example-project-evosuite/left/target/classes/'
-        self.mergeDir = '/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/apiguardian-api-1.0.0.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/commons-lang3-3.0.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/hamcrest-core-1.3.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/junit-4.13-beta-1.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/junit-jupiter-api-5.0.3.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/junit-platform-commons-1.0.3.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/dependency/opentest4j-1.0.0.jar:/home/jprm/Documents/IC/example-project-evosuite/merge/target/classes/'
+        self.dRegCp = None #base
+        self.classes_dir = None #left
+        self.mergeDir = None #merge
 
         self.config = get_config()
         self.evosuite_diff_params = None
@@ -43,13 +43,13 @@ class evotest:
         self.path_hash_csv = self.config["path_hash_csv"]
         self.path_output_csv = self.config["path_output_csv"]
 
-    def gen_evosuite_diff(self):
+    def gen_evosuite_diff(self, scenario):
         evosuite = Evosuite(
             java=self.java,
             classpath=self.classes_dir,
             sut_class=self.sut_class,
             params=self.evosuite_diff_params,
-            tests_src=self.tests_dst
+            tests_src=self.tests_dst + '/' + self.project.get_project_name() + '/' + scenario.get_merge_hash()
         )
         self.suite_evosuite_diff = evosuite.generate_differential(self.dRegCp)
         return self.suite_evosuite_diff
@@ -59,16 +59,10 @@ class evotest:
         res = junit.run_with_mutant(self.suite_evosuite_diff, sut_class, mutant_dir)
         return res
 
-    def create_nimrod_result(test_result, differential, test_tool, is_equal_coverage=False):
-        return NimrodResult(
-            test_result.fail_tests == 0 and not test_result.timeout,
-            test_result.fail_tests > 0 or test_result.timeout,
-            test_result.coverage, differential, test_result.timeout, test_tool, is_equal_coverage)
+    def analyze_commits(self, scenario):
 
-    def analyze_commits(self, commit):
-
-        data = [(commit.get_base_hash(), "base"), (commit.get_left_hash(), "left"), (commit.get_right_hash(), "right"), (commit.get_merge_hash(), "merge")]
-        self.sut_class = commit.get_sut_class()
+        data = [(scenario.get_base_hash(), "base"), (scenario.get_left_hash(), "left"), (scenario.get_right_hash(), "right"), (scenario.get_merge_hash(), "merge")]
+        self.sut_class = scenario.get_sut_class()
         for hash in data:
             self.project.checkout_on_commit(hash[0])
             a = self.maven.compile(self.project.get_path_local_project(), 120, clean=True, install=True)
@@ -95,11 +89,11 @@ class evotest:
 
         return finalPath
 
-    def writeOutputCsv(self, outputBase, outputLeft, outputMerge, commit):
+    def writeOutputCsv(self, outputBase, outputLeft, outputMerge, scenario):
 
-        output=[commit.get_merge_hash(), "False"]
+        output=[scenario.get_merge_hash(), "False"]
         if outputBase[1] and not outputLeft[1] and outputMerge[1]:
-            output = [commit.get_merge_hash(), "True"]
+            output = [scenario.get_merge_hash(), "True"]
 
         with open(self.path_output_csv, 'a') as fd:
             writer = csv.writer(fd)
@@ -111,22 +105,21 @@ class evotest:
 if __name__ == '__main__':
 
     evo = evotest()
-    #evo.writeOutputCsv(["aasas","lol"])
 
     merge = MergeScenario(evo.project.get_path_local_project, evo.path_hash_csv)
     merge_scenarios = merge.get_merge_scenarios()
     for scenario in merge_scenarios:
         evo.analyze_commits(scenario)
-        #print(scenario.get_sut_class())
 
-        evo.dRegCp = evo.generateDependenciesPath(scenario,"base")
-        evo.classes_dir = evo.generateDependenciesPath(scenario,"left")
-        evo.mergeDir = evo.generateDependenciesPath(scenario,"merge")
+        evo.dRegCp = evo.generateDependenciesPath(scenario, "base")
+        evo.classes_dir = evo.generateDependenciesPath(scenario, "left")
+        evo.mergeDir = evo.generateDependenciesPath(scenario, "merge")
 
-        thread_evosuite_diff = threading.Thread(target=evo.gen_evosuite_diff)
+        #thread_evosuite_diff = threading.Thread(target=evo.gen_evosuite_diff)
         print("waiting analisys finish")
-        thread_evosuite_diff.start()
-        thread_evosuite_diff.join()
+        evo.gen_evosuite_diff(scenario)
+        #thread_evosuite_diff.start()
+        #thread_evosuite_diff.join()
         print("ended")
 
         time.sleep(5)
@@ -164,62 +157,3 @@ if __name__ == '__main__':
 
 
 
-'''
-    evo = evotest()/
-    mvn = evo.maven
-    project = GitProject("/home/jprm/Documents/IC/ex/example-project-evosuite")
-
-    projects_folder = "/home/jprm/Documents/IC/ex/Projects/"
-
-    hash_type = "base"
-    project.checkout_on_commit("78d4c577691721383765e82f2e2397a189a7f041")
-
-    res = mvn.clean(project.get_path_local_project(), 60)
-    res2= mvn.save_dependencies(project.get_path_local_project())
-    print(res2)
-
-    dst = projects_folder +project.get_project_name()+"/"+hash_type;
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-
-    base_dir = shutil.copytree(project.get_path_local_project(), dst)
-    print(base_dir)
-
-    project.checkout_on_commit("c8f232ee75c4ec2928953d19c3479dbb5a4ba12b")
-    hash_type = "left"
-
-    res = mvn.clean(project.get_path_local_project(), 60)
-    res2= mvn.save_dependencies(project.get_path_local_project())
-    print(res2)
-
-    dst = projects_folder +project.get_project_name()+"/"+hash_type;
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    left_dir = shutil.copytree(project.get_path_local_project(), dst)
-
-    project.checkout_on_commit("f89d96f9e05a663908f4bdd1c408e2d20be9ed0f")
-
-    hash_type = "merge"
-
-    res = mvn.clean(project.get_path_local_project(), 60)
-    res2= mvn.save_dependencies(project.get_path_local_project())
-    print(res2)
-
-    dst = projects_folder +project.get_project_name()+"/"+hash_type;
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    merge_dir = shutil.copytree(project.get_path_local_project(), dst)
-
-
-
-    #if test_result.fail_tests > 0 or test_result.timeout:
-    #    results = evo.create_nimrod_result(test_result, True, 'evosuite')
-
-
-
-    test_result = evo.try_evosuite_diff(evo.classes_dir, evo.tests_src,evo.sut_class)
-
-    if test_result.fail_tests > 0 or test_result.timeout:
-        results[mutant.mid] = self.create_nimrod_result(test_result,
-                                                        True, 'evosuite')
-'''
