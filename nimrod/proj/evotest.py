18c614f23da5a9fde88b67de854f92a0a0adc933
leusonmario <leusonmario@gmail.com>
2019-11-16 03:11:13 +0100

    Extraction of tool setup process (randoop)

--- nimrod/proj/evotest.py
+++ nimrod/proj/evotest.py
@@ -10,6 +10,7 @@ from nimrod.report.output_report import Output_report
 from nimrod.proj.project_dependencies import Project_dependecies
 from nimrod.setup_tools.evosuite_setup import Evosuite_setup
 from nimrod.setup_tools.evosuite_diff_setup import Evosuite_Diff_setup
+from nimrod.setup_tools.randoop_setup import Randoop_setup
 
 NimrodResult = namedtuple('NimrodResult', ['maybe_equivalent', 'not_equivalent',
                                            'coverage', 'differential',
@@ -24,6 +25,7 @@ class evotest:
 
         self.evosuite_setup = Evosuite_setup()
         self.evosuite_diff_setup = Evosuite_Diff_setup()
+        self.randoop_setup = Randoop_setup()
 
         self.suite_evosuite = None
         self.evosuite_params = None
@@ -124,23 +126,31 @@ if __name__ == '__main__':
                 result_evodiff_right = evo.evosuite_diff_setup.exec_evosuite_diff_jar(evo, merge, row[10], row[12], row[13])
                 result_evosuite_left = evo.evosuite_setup.exec_evosuite_jar(evo, merge, row[10], row[11], row[13])
                 result_evosuite_right = evo.evosuite_setup.exec_evosuite_jar(evo, merge, row[10], row[12], row[13])
+                result_randoop_left = evo.randoop_setup.exec_randoop_jar(evo, merge, row[10], row[11], row[13])
+                result_randoop_right = evo.randoop_setup.exec_randoop_jar(evo, merge, row[10], row[12], row[13])
 
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge, "evosuite-diff", "left", result_evodiff_left)
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(),merge, "evosuite-diff", "right", result_evodiff_right)
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(),merge, "evosuite", "left", result_evosuite_left)
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(),merge, "evosuite", "right", result_evosuite_right)
+                evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge, "randoop", "left", result_randoop_left)
+                evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge, "randoop", "right", result_randoop_right)
 
             else:
                 evo = evotest(row[8], row[9], row[0])
                 merge = MergeScenario(evo.project_dep.project.get_path_local_project, row)
                 evo.project_dep.compile_commits(merge)
 
-                result_evosuite_left = evo.evosuite_setup.exec_evosuite(evo, merge, "left")
-                result_evosuite_right = evo.evosuite_setup.exec_evosuite(evo, merge, "right")
                 result_evodiff_left = evo.evosuite_diff_setup.exec_evosuite_diff(evo, merge, "left")
                 result_evodiff_right = evo.evosuite_diff_setup.exec_evosuite_diff(evo, merge, "right")
+                result_evosuite_left = evo.evosuite_setup.exec_evosuite(evo, merge, "left")
+                result_evosuite_right = evo.evosuite_setup.exec_evosuite(evo, merge, "right")
+                result_randoop_left = evo.randoop_setup.exec_randoop(evo, merge, "left")
+                result_randoop_right = evo.randoop_setup.exec_randoop(evo, merge, "right")
 
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge, "evosuite-diff", "left", result_evodiff_left)
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge, "evosuite-diff", "right", result_evodiff_right)
                 evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge,"evosuite", "left", result_evosuite_left)
-                evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge,"evosuite", "right", result_evosuite_right)
+                evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge,"evosuite", "right", result_evosuite_right)                
+                evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge,"randoop", "left", result_randoop_left)
+                evo.output_report.write_output_results(evo.project_dep.project.get_project_name(), merge,"randoop", "right", result_randoop_right)
---
+++ nimrod/setup_tools/randoop_setup.py
@@ -0,0 +1,84 @@
+from nimrod.tools.randoop import Randoop
+from nimrod.tools.safira import Safira
+from nimrod.tools.junit import JUnit
+from nimrod.setup_tools.behaviour_check import Behaviour_change
+
+class Randoop_setup:
+
+    def __init__(self):
+        self.suite_randoop = None
+        self.randoop_params = None
+        self.behaviour_change = Behaviour_change()
+
+    def gen_randoop(self, scenario, project_dep):
+        randoop = Randoop(
+            java=project_dep.java,
+            classpath=project_dep.classes_dir,
+            tests_src=project_dep.tests_dst + '/' + project_dep.project.get_project_name() + '/' + scenario.merge_scenario.get_merge_hash(),
+            sut_class=project_dep.sut_class,
+            params=self.randoop_params
+        )
+        safira = Safira(java=project_dep.java, classes_dir=project_dep.classes_dir, mutant_dir=project_dep.dRegCp)
+        if "Bisect" in project_dep.sut_class:
+            self.suite_randoop = randoop.generate()
+        else:
+            self.suite_randoop = randoop.generate_with_impact_analysis(safira)
+            if "Simulator" in project_dep.sut_class:
+                import distutils.dir_util
+                distutils.dir_util.copy_tree("./config/", randoop.suite_dir + "/config/")
+        return self.suite_randoop
+
+    def try_randoop(self, classes_dir, sut_class, mutant_dir, project_dep):
+        junit = JUnit(java=project_dep.java, classpath=classes_dir)
+        return (junit.run_with_mutant(self.suite_randoop, sut_class, mutant_dir)
+                if self.suite_randoop else None)
+
+    def exec_randoop(self, evo, scenario, parent):
+        conflict_info = []
+        try:
+            evo.project_dep.dRegCp = evo.project_dep.generate_dependencies_path(scenario, "base")
+            evo.project_dep.classes_dir = evo.project_dep.generate_dependencies_path(scenario, parent)
+            evo.project_dep.mergeDir = evo.project_dep.generate_dependencies_path(scenario, "merge")
+
+            conflict_info = self.exec_randoop_all(evo, scenario)
+        except:
+            print("Some project versions could not be evaluated")
+
+        return conflict_info
+
+    def exec_randoop_jar(self, evo, scenario, jarBase, jarParent, jarMerge):
+        conflict_info = []
+        try:
+            evo.project_dep.dRegCp = jarBase
+            evo.project_dep.classes_dir = jarParent
+            evo.project_dep.mergeDir = jarMerge
+            evo.project_dep.sut_class = scenario.merge_scenario.get_sut_class()
+
+            conflict_info = self.exec_randoop_all(evo, scenario)
+        except:
+            print("Some project versions could not be evaluated")
+
+        return conflict_info
+
+    def exec_randoop_all(self, evo, scenario):
+        conflict_info = []
+        try:
+            path_suite = self.gen_randoop(scenario, evo.project_dep)
+
+            test_result_base = self.try_randoop(evo.project_dep.classes_dir, evo.project_dep.sut_class,
+                                                evo.project_dep.dRegCp, evo.project_dep)  # fail on base - passing tests 0 2 7
+            test_result_parent = self.try_randoop(evo.project_dep.classes_dir, evo.project_dep.sut_class, evo.project_dep.classes_dir, evo.project_dep)  # pass on left
+            test_result_merge = self.try_randoop(evo.project_dep.classes_dir, evo.project_dep.sut_class,
+                                                 evo.project_dep.mergeDir, evo.project_dep)  # fail on merge - passing tests 0 2 7
+
+            conflict_info.append(
+                self.behaviour_change.check_different_test_results_for_commit_pair(test_result_base, test_result_parent, path_suite))
+            conflict_info.append(
+                self.behaviour_change.check_different_test_results_for_commit_pair(test_result_merge, test_result_parent, path_suite))
+            conflict_info.append(self.behaviour_change.check_different_test_results_for_merge_scenario(test_result_base, test_result_parent,
+                                                                                      test_result_merge, path_suite))
+
+        except:
+            print("Some project versions could not be evaluated")
+
+        return conflict_info
