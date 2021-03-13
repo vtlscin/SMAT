a945c6a850138557128858abbd3fcd760512463d
vinicius leite <viniciusthiago2@hotmail.com>
2021-03-13 11:39:56 -0300

    Ajuste na chamada da classe Java

--- nimrod/tools/jacoco.py
+++ nimrod/tools/jacoco.py
@@ -1,18 +1,16 @@
 from nimrod.tools.bin import JACOCOCLI, JACOCOAGENT
 from nimrod.utils import generate_classpath
 
-TIMEOUT = 80
 
 class Jacoco:
-    def __init__(self, java, classpath):
+    def __init__(self, java):
         self.java = java
-        self.classpath = classpath
 
-    #Run java -jar jacococli.jar instrument project_jar --dest dest_jar_instrumented
+    # Run java -jar jacococli.jar instrument project_jar --dest dest_jar_instrumented
     def execIntrumentJar(self, projectJar, destJarinstrumented):
         params = [
             '-jar', JACOCOCLI,
             'instrument', projectJar,
             '--dest', destJarinstrumented
         ]
-        self.java.simple_exec_java(params)
+        return self.java.simple_exec_java(*params)
