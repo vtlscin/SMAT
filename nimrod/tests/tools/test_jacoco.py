2e34a913a60d1940c1cf54a8a9866a5db3927394
vinicius leite <viniciusthiago2@hotmail.com>
2021-03-18 21:01:11 -0300

    Criacao de novo metodo de teste para comandos do jacoco.

--- nimrod/tests/tools/test_jacoco.py
+++ nimrod/tests/tools/test_jacoco.py
@@ -11,12 +11,27 @@ class TestJacoco(TestCase):
     def setUp(cls):
         cls.java_home = get_config()['java_home']
         cls.java = Java(cls.java_home)
+        cls.jacoco = Jacoco(cls.java)
 
-    # Teste do comando java -jar jacococli.jar instrument "jar_a_ser_modificado" --dest "local_jar_instrumetado"
+    # Teste do comando java -jar jacococli.jar instrument "jar_a_ser_modificado" --dest "local_jar_instrumentado"
     def test_instrument(self):
-        jacoco = Jacoco(self.java)
 
         jarSemModificacaoes = "/home/vinicius/Documentos/UFPE/TCC/mergedataset/cloud-slang/20bac30d9bd76569aa6a4fa1e8261c1a9b5e6f76/original/base/cloudslang-all-0.7.50-SNAPSHOT-jar-with-dependencies.jar"
         destinoJarInstrumentado = "/home/vinicius/Documentos/UFPE/TCC/Resultados/CloudSlang/cloudslang-all/dest"
 
-        jacoco.execIntrumentJar(jarSemModificacaoes, destinoJarInstrumentado)
+        self.jacoco.execInstrumentJar(jarSemModificacaoes, destinoJarInstrumentado)
+
+    def test_runTest(self):
+
+        projectJar = "/home/vinicius/Documentos/UFPE/TCC/Resultados/CloudSlang/cloudslang-all/dest/cloudslang-all-0.7.50-SNAPSHOT-jar-with-dependencies.jar"
+        suiteClass = "/home/vinicius/Documentos/UFPE/TCC/Resultados/CloudSlang/cloudslang-all/Class/"
+        test_class = "RegressionTest0"
+
+        self.jacoco.createJacocoExec(projectJar, suiteClass, test_class)
+
+    def test_generateReport(self):
+        jacocoExecDir = "/home/vinicius/Documentos/UFPE/TCC/SMAT/nimrod/tests/tools/jacoco.exec"
+        classFile = "/home/vinicius/Documentos/UFPE/TCC/Projetos/cloud-slang/cloudslang-all/target/classes/io/cloudslang/lang/api"
+        csvFile = "/home/vinicius/Documentos/UFPE/TCC/Resultados/CloudSlang/cloudslang-all/dest/report.csv"
+
+        self.jacoco.generateReport(jacocoExecDir, classFile, csvFile)
--- nimrod/tools/jacoco.py
+++ nimrod/tools/jacoco.py
@@ -1,4 +1,4 @@
-from nimrod.tools.bin import JACOCOCLI, JACOCOAGENT
+from nimrod.tools.bin import JACOCOCLI, JACOCOAGENT, JUNIT, HAMCREST
 from nimrod.utils import generate_classpath
 
 
@@ -7,10 +7,41 @@ class Jacoco:
         self.java = java
 
     # Run java -jar jacococli.jar instrument project_jar --dest dest_jar_instrumented
-    def execIntrumentJar(self, projectJar, destJarinstrumented):
+    def execInstrumentJar(self, projectJar, destJarinstrumented):
         params = [
             '-jar', JACOCOCLI,
             'instrument', projectJar,
             '--dest', destJarinstrumented
         ]
+        return self.execJava(*params)
+
+    # projectJar = jar instrumentado do projeto
+    # suite_class = local do arquivo class da suite de testes
+    # test_class = classe de teste
+    def createJacocoExec(self, projectJar, suite_class, test_class):
+        classpath = generate_classpath([
+            JUNIT, HAMCREST, JACOCOAGENT,
+            suite_class, projectJar
+        ])
+        params = (
+            '-classpath', classpath,
+            'org.junit.runner.JUnitCore', test_class
+        )
+
+        return self.execJava(*params)
+
+    # jacocoExecDir = local do arquivo jacocoExec
+    # classFiles = local do arquivo class da classe alvo dos testes.
+    # nameCsvFile = nome do arquivo csv gerado com o report dos testes.
+    def generateReport(self, jacocoExecDir, classFiles, nameCsvFile):
+        params = [
+            '-jar', JACOCOCLI,
+            'report', jacocoExecDir,
+            '--classfiles', classFiles,
+            '--csv', nameCsvFile
+        ]
+
+        return self.execJava(*params)
+
+    def execJava(self, *params):
         return self.java.simple_exec_java(*params)
