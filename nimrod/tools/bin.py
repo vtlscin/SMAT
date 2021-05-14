d916e95b42045fbf8413fc964f24791ff906c2fa
ToniMaciel <jaam@cin.ufpe.br>
2021-05-13 21:59:04 -0300

    Adding the original randoop jar that makes reports

--- nimrod/tools/bin.py
+++ nimrod/tools/bin.py
@@ -9,5 +9,5 @@ EVOSUITE_RUNTIME = os.sep.join([PATH, 'bin',
                                 'evosuite-standalone-runtime-1.0.6.jar'])
 MOD_RANDOOP = os.sep.join([PATH, 'bin', 'modified-randoop-all-4.2.5.jar'])
 JMOCKIT = os.sep.join([PATH, 'bin', 'jmockit-1.40-marcio.1.jar'])
-RANDOOP = os.sep.join([PATH, 'bin', 'randoop-all-4.2.2.jar'])
+RANDOOP = os.sep.join([PATH, 'bin', 'randoop-all-4.2.5.jar'])
 SAFIRA = os.sep.join([PATH, 'bin', 'safira.jar'])
