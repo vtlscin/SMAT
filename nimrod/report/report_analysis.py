e8e36f5c4fbf0307c7e12174d7897e1c9347afe1
ToniMaciel <jaam@cin.ufpe.br>
2021-05-13 22:38:31 -0300

    Adding comments in class

--- nimrod/report/report_analysis.py
+++ nimrod/report/report_analysis.py
@@ -28,13 +28,13 @@ class Report_Analysis:
         objects = self.objects_report_comparison(path_suite_original, path_suite_modified)
 
         if len(methods) == 0 or len(objects) == 0:
-            print("It wasn't possible to compare the suites " + str(path_suite_original) + " and " + str(
-                path_suite_modified))
+            print("It wasn't possible to compare the suites " + str(path_suite_original) + " and " + str(path_suite_modified))
         else:
             self.write_comparison(path_suite_original, path_suite_modified, methods, objects)
 
     def methods_report_comparison(self, path_suite_original, path_suite_modified):
-        methods_data = {}
+        methods_data = {}  # A dictionary in which the method name is the key and the value returned is a list with
+        # the number of times that the method was called for original randoop and randoop--
 
         try:
             methods_report_file = open(path_suite_original + "/methods_report.csv")
@@ -42,11 +42,9 @@ class Report_Analysis:
             methods_report_file.close()
 
             lines = methods_report.split("\n")
-            for line in lines[
-                        1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
+            for line in lines[1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
                 cells = re.split("(?<=\"),", line)
-                method_map = {cells[0]: [cells[1],
-                                         0]}  # A dictionary in which the method name is the key and the number of times that his was called is the value
+                method_map = {cells[0]: [cells[1], 0]}
                 methods_data.update(method_map)
         except:
             print("Error on methods_report.csv in " + str(path_suite_original))
@@ -58,14 +56,12 @@ class Report_Analysis:
             methods_report_file.close()
 
             lines = methods_report.split("\n")
-            for line in lines[
-                        1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
+            for line in lines[1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
                 cells = re.split("(?<=\"),", line)
-                if cells[0] in methods_data:
+                if cells[0] in methods_data:  # If the method signature is already in the dictionary, just change the value in the list
                     methods_data.get(cells[0])[1] = cells[1]
                 else:
-                    method_map = {cells[0]: [0, cells[
-                        1]]}  # A dictionary in which the method name is the key and the number of times that his was called is the value
+                    method_map = {cells[0]: [0, cells[1]]}
                     methods_data.update(method_map)
         except:
             print("Error on methods_report.csv in " + str(path_suite_modified))
@@ -74,7 +70,9 @@ class Report_Analysis:
         return methods_data
 
     def objects_report_comparison(self, path_suite_original, path_suite_modified):
-        objects_data = {}
+        objects_data = {}  # A dictionary the class of the object is the key and the value returned is a list with
+        # the number of times that a objects from the class were created and how many of them are different from each
+        # other, in that order, from randoop and randoop--
 
         try:
             objects_report_file = open(path_suite_original + "/objects_report.csv")
@@ -82,11 +80,9 @@ class Report_Analysis:
             objects_report_file.close()
 
             lines = objects_report.split("\n")
-            for line in lines[
-                        1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
+            for line in lines[1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
                 cells = line.split(",")
-                object_map = {cells[0]: [cells[1], cells[2], 0,
-                                         0]}  # A dictionary in which the class of the object is the key and the value is a list with both the number of times that a objects from the class were created and how many of them are different from each other
+                object_map = {cells[0]: [cells[1], cells[2], 0, 0]}
                 objects_data.update(object_map)
         except:
             print("Error on objects_report.csv in " + str(path_suite_original))
@@ -98,15 +94,13 @@ class Report_Analysis:
             objects_report_file.close()
 
             lines = objects_report.split("\n")
-            for line in lines[
-                        1:-1]:  # Exclude the first one because contains the headers and the last one because it is a empty line
+            for line in lines[1:-1]: # Exclude the first one because contains the headers and the last one because it is a empty line
                 cells = line.split(",")
-                if cells[0] in objects_data:
+                if cells[0] in objects_data: # If the class is already in the dictionary, just change the value in the list
                     objects_data.get(cells[0])[2] = cells[1]
                     objects_data.get(cells[0])[3] = cells[2]
                 else:
-                    object_map = {cells[0]: [0, 0, cells[1], cells[
-                        2]]}  # A dictionary in which the class of the object is the key and the value is a list with both the number of times that a objects from the class were created and how many of them are different from each other
+                    object_map = {cells[0]: [0, 0, cells[1], cells[2]]}
                     objects_data.update(object_map)
         except:
             print("Error on methods_report.csv in " + str(path_suite_modified))
@@ -115,8 +109,8 @@ class Report_Analysis:
         return objects_data
 
     def write_comparison(self, path_suite_original, path_suite_modified, methods, objects):
-        suites_path = path_suite_original[:path_suite_original.rfind("/")]
-        reports_path = suites_path + "/reports"# Change that later
+        suites_path = path_suite_original[:path_suite_original.rfind("/")]  # Get the directory where the test suites are
+        reports_path = suites_path + "/reports"
 
         if os.path.isdir(reports_path) is False:
             os.mkdir(reports_path)
@@ -140,8 +134,7 @@ class Report_Analysis:
                     relative = (sub / number_original) * 100
                     relative = str(round(relative, 2)).replace(".", ",")
 
-                text = key + "," + str(number_original) + "," + str(number_modified) + "," + str(
-                    sub) + "," + "\"" + relative + "\"%" + "\n"
+                text = key + "," + str(number_original) + "," + str(number_modified) + "," + str(sub) + "," + "\"" + relative + "\"%" + "\n"
                 output_file.write(text)
 
             output_file.close()
@@ -169,23 +162,9 @@ class Report_Analysis:
                     relative_unique = (sub_unique / number_original_unique) * 100
                     relative_unique = str(round(relative_unique, 2)).replace(".", ",")
 
-                text = key + "," + str(number_original) + "," + str(number_modified) + "," + str(
-                    sub) + "," + "\"" + relative + "\"%" + "," + str(number_original_unique) + "," + str(
-                    number_modified_unique) + "," + str(sub_unique) + "," + "\"" + relative_unique + "\"%" + "\n"
+                text = key + "," + str(number_original) + "," + str(number_modified) + "," + str(sub) + "," + "\"" + relative + "\"%" + "," + str(number_original_unique) + "," + str(number_modified_unique) + "," + str(sub_unique) + "," + "\"" + relative_unique + "\"%" + "\n"
                 output_file.write(text)
 
             output_file.close()
 
         print("The analysis of suites " + suite_name_original + " and " + suite_name_modified + " was done")
-
-
-
-
-
-
-
-
-
-
-
-
